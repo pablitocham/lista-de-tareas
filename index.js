12e4a8387cab411a00528314a4209037ddaa9081
Juan Pablo Chamorro <jpablocham@gmail.com>
2024-10-22 13:58:59 -0300

    lista

---
+++ index.js
@@ -0,0 +1,68 @@
+function agregarTarea() {
+    let nuevaTareasTexto = document.getElementById("nuevasTareas").value
+    if (nuevaTareasTexto === "") {
+        Swal.fire({
+            title: "Debe ingresar una tarea",
+            icon: "warnig"
+          });
+        return
+    }
+
+    let nuevasTareas = document.createElement("li")
+    nuevasTareas.textContent = nuevaTareasTexto + " "
+    let botonEliminar = document.createElement("button")
+    botonEliminar.textContent = "Eliminar"
+    botonEliminar.onclick = function () {
+        nuevasTareas.remove()
+        eliminarTareas(nuevaTareasTexto)
+    }
+
+    nuevasTareas.appendChild(botonEliminar)
+    document.getElementById("lista").appendChild(nuevasTareas)
+    guardarTareas(nuevaTareasTexto)
+    document.getElementById("nuevasTareas").value = ""
+}
+
+
+function guardarTareas(tarea) {
+    let tareas = obtenerTareas()
+    tareas.push(tarea)
+    localStorage.setItem("tareas", JSON.stringify(tareas))
+}
+
+function eliminarTareas(tarea) {
+    let tareas = obtenerTareas()
+    tareas = tareas.filter(t => t !== tarea)
+    localStorage.setItem("tareas", JSON.stringify(tareas))
+}
+
+function obtenerTareas() {
+    let tareas = localStorage.getItem("tareas")
+    return tareas ? JSON.parse(tareas) : []
+}
+
+function cargarTareas() {
+    let tareas = obtenerTareas()
+    tareas.forEach(tarea => {
+        let nuevasTareas = document.createElement("li")
+        nuevasTareas.textContent = tarea + " "
+        let botonEliminar = document.createElement("button")
+        botonEliminar.textContent = "Eliminar"
+        botonEliminar.onclick = function () {
+            nuevasTareas.remove()
+            eliminarTareas(tarea)
+        }
+        nuevasTareas.appendChild(botonEliminar)
+        document.getElementById("lista").appendChild(nuevasTareas)
+    })
+
+}
+
+window.onload = function () {
+    cargarTareas()
+    document.getElementById("nuevasTareas").addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            agregarTarea()
+        }
+    })
+}
